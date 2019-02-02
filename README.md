1d8a7295ab23f0f017a4418a2319270dd857f48c
Sean Wesenberg <wookets@gmail.com>
2019-02-01 22:25:22 -0600

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Thorntail Example
+
+```
+
+```
---
+++ src/main/java/wookets/thorntail/ThorntailApplication.java
@@ -0,0 +1,8 @@
+package wookets.thorntail;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/")
+public class ThorntailApplication extends Application {
+}
